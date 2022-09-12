a7da71c22ef23f70130fc2711f4b9fa523f5b6a2
Adewalepro <adewale78789@gmail.com>
2022-09-12 16:52:08 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+ *
+ *main - write a program that print alphabet in lowercase
+ *
+ *Return: 0 always
+ *
+ */
+#include <stdio.h>
+int main(void)
+{  
+	char c;
+	for (c = 'a' ; c <= 'z' c++)
+	{
+		putchar (c);
+	}
+	  return(0);
+}
