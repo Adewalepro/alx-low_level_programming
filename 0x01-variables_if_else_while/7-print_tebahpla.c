46320632d7acdedc094e891a104b0b8ad9e5f75a
Adewalepro <adewale78789@gmail.com>
2022-09-12 19:53:29 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - print lowercase alphabet reverse
+*
+*
+*Return: 0 always
+*/
+int main(void)
+{char l;
+for (l = 'z'; l >= 'a'; l--)
+putchar(l);
+putchar('\n');
+return (0); }
