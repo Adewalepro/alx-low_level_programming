0d3434c25aea4884c5e43e73a0c0f9d9f2d75f04
Adewalepro <adewale78789@gmail.com>
2022-09-12 19:16:53 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print all letters except q and e
+*
+*
+*Return: 0 always
+*/
+int main(void)
+{char s;
+for (s = 'a'; s <= 'z'; s++)
+{
+if (s != 'e' && s != 'q')
+putchar(s); }
+putchar('\n')
+return (0); }
