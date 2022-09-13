082e1e37508cb959a58b1e55f0af3493c4c6a95d
Adewalepro <adewale78789@gmail.com>
2022-09-12 19:28:08 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - print all single digit of base 10
+*
+*
+*Return: 0 always
+*/
+int main(void)
+{int numb;
+for (num = 0; num < 10; num++)
+print("%d", num);
+print('\n');
+return (0); }
