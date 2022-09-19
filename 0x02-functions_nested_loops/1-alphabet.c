1f00f0ea36624ff76d3c07ddd9b84ad27ab1c5ca
Adewalepro <adewale78789@gmail.com>
2022-09-19 14:43:40 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
 *description: print_alphabet in lowercase putchar
 *
 */
-void print_alphabet(void);
+void print_alphabet(void)
 { char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
